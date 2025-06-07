352168a433f6872e6bd5cc01283381f92e2fc43f
[aateka limbaliya] <[aatekalimbaliya910@gmail.com]>
2025-06-07 18:59:53 +0530

    changed vite config file

--- src/pages/LifeStyleBlog.jsx
+++ src/pages/LifeStyleBlog.jsx
@@ -26,12 +26,14 @@ function LifeStyleBlog() {
     ]
 
   return (
-    <div>
-      <div className="mb-10">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">LifeStyle blog</h1>
+   <div>
+    <div className='bg-orange-100 text-white py-16 center'>
+    <div className="container mx-auto px-4 text-center border-gray-600">
+      <h1 className="text-3xl font-bold text-orange-800 mb-4">Life Style Blog</h1>
         <p className="text-lg text-gray-600">Discover tips, workouts, and advice to help you achieve your fitness goals and maintain a healthy lifestyle.</p>
+        </div> 
       </div>
-      
+      <br />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {LifeStylePosts.map(post => (
           <BlogPost key={post.id} {...post} />
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/blogg-web",
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
 })
